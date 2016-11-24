62bda5d081db1a9523c613232ea5d0270f54bac8
Cao Yuhao <1174478646@qq.com>
2016-11-24 19:04:50 +0800

    aaa

--- main.py
+++ main.py
@@ -5,9 +5,13 @@ import re
 from sqlHelper import SqlHelper  # sqlHelper
 from scrabHelper import ScrabHelper
 from dateHelper import DateHelper
+from sys import argv
 
+dmId = argv[1]
 
-dmId = 1
+if dmId != '1':
+    print 'wrong'
+    quit()
 
 scrabJson = {"data": [{
                           "scrabId": 1,
@@ -26,18 +30,17 @@ def doSave(json):
     scrab_result = json.get('scrab_result')
     data_time = json.get('data_time')
 
-    '''
     rowCount = SqlHelper.ExecuteNonQuery(
         "insert into jimi_radar_result (scrab_id,processed_clue,scrab_result,data_time,insert_time) values('%d','%s','%s','%s','%s')" % (
             scrabId, processed_clue, scrab_result, data_time, DateHelper.getDateNowStr())
     )
-    print rowCount
-    '''
+    # print rowCount
 
 
 def doScrab(json):
     # print json
-    defaultScrabPageNum = json.get('defaultScrabPageNum') or 10
+    startPageNum = 1  # 可能是0
+    defaultScrabPageNum = json.get('defaultScrabPageNum') or 5
     scrabId = json['scrabId']
     clue = json['clue']
 
@@ -48,19 +51,24 @@ def doScrab(json):
         matches = re.findall(r"/(\d+)[.]{1}html", clue)
         proId = matches[0]
 
-        startPageNum = 1  # 可能是0
-
         interfaceWithoutParas = 'http://cosme.pclady.com.cn/common/2013/solr_comment_list.jsp?&pageType=1&attitude=&status=&lvl=&ageBegin=&ageEnd=&skin=&stp=0&atp=0&type=0&pageNum=%d&id=' + str(
             proId)
 
         # 第一层for循环 循环接口
-        while (True):
+        while True:
+
+            # 如果有默认爬取页数
+            if defaultScrabPageNum and startPageNum > defaultScrabPageNum:
+                print '0'
+                return
+
             interface = interfaceWithoutParas % (startPageNum)
 
             soup = ScrabHelper.getSoupFromInterface(interface, 'gb2312')
 
             divs = soup.find_all('div', 'userCmtList')[0].find_all('ul')[0].find_all('div', 'userCmt')
             if len(divs) == 0:
+                print '无数据'
                 return
             else:
                 for div in divs:
@@ -83,23 +91,38 @@ def doScrab(json):
         matches = re.findall(r"product/(\d+)/", clue)
         proId = matches[0]
 
-        startPageNum = 1  # 可能是0
         interfaceWithoutParas = 'http://product.kimiss.com/product/%s/%d/'
-        interface = interfaceWithoutParas % (proId, startPageNum)
-        print interface
-        soup = ScrabHelper.getSoupFromInterface(interface, 'utf-8')
-        divs = soup.find_all('ul', 'f_remark_list')[0].find_all('div', 'f_re_rig')
-        for div in divs:
-            content = div.find_all('p', 'com_p')[0].string
-            date = div.find_all('div', 'f_re_time')[0].find_all('cite')[0].string
-            print str(1) + '-------------' + content
-            print str(1) + '-------------' + date
-            doSave(
-                {'scrabId': scrabId,
-                 'processed_clue': proId,
-                 'scrab_result': content,
-                 'data_time': date.strip(),
-                })
+
+
+        # 第一层for循环 循环接口
+        while True:
+            # 如果有默认爬取页数
+            if defaultScrabPageNum and startPageNum > defaultScrabPageNum:
+                print '0'
+                return
+
+            interface = interfaceWithoutParas % (proId, startPageNum)
+
+            soup = ScrabHelper.getSoupFromInterface(interface, 'utf-8')
+
+            divs = soup.find_all('ul', 'f_remark_list')[0].find_all('div', 'f_re_rig')
+            if len(divs) == 0:
+                print '无数据'
+                return
+            else:
+                for div in divs:
+                    content = div.find_all('p', 'com_p')[0].string
+                    date = div.find_all('div', 'f_re_time')[0].find_all('cite')[0].string
+                    # print str(1) + '-------------' + content
+                    # print str(1) + '-------------' + date
+                    doSave(
+                        {'scrabId': scrabId,
+                         'processed_clue': proId,
+                         'scrab_result': content,
+                         'data_time': date.strip(),
+                        })
+
+            startPageNum += 1
 
 # 主程 开始遍历scrabID爬数据
 '''
@@ -111,7 +134,7 @@ for i in range(len(scrabArray)):
     doScrab(scrabId, clue)
 '''
 
-ele = scrabArray[0]
+ele = scrabArray[1]
 scrabId = ele['scrabId']
 clue = ele['clue']
 doScrab({'scrabId': scrabId,
